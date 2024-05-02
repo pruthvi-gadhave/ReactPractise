10f55cdd44f6920052eed96673f6b3d87f9f56d0
PRUTHVI GADHAVE <pruthvi.github25@gmail.com>
2024-05-02 19:45:49 +0530

    add todo completed

--- react-practise/todo-app/src/App.js
+++ react-practise/todo-app/src/App.js
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import AppName from "./components/AppName";
 import ToDoForm from "./components/ToDoForm";
 import ToDoItems from "./components/ToDoItems";
 
-const todoList = [
-  {
-    name : "Buy Milks" ,
-    dueDate : "04/04/2023"
-  },
-  {
-    name : "Buy Butter" ,
-    dueDate : "05/04/2023"
-  },
-]
+
+
 
 function App () {
+  
+// const todoList = [
+//   {
+//     name : "Buy Milks" ,
+//     dueDate : "04/04/2023"
+//   },
+//   {
+//     name : "Buy Butter" ,
+//     dueDate : "05/04/2023"
+//   },
+// ]
+
+
+const [todoList ,setTodoList ] = useState([]) ; 
+
+  const handleAddTodo = (toDo) => {
+    console.log(`toDoItem = ${toDo.name} , date = ${toDo.dueDate}`);
+    const newToDos = [...todoList , toDo];
+    console.log(newToDos);
+    setTodoList(newToDos);
+    
+  }
   return (<>
  <AppName/>
-  <ToDoForm/>
+  <ToDoForm handleAddTodo={handleAddTodo}/>
   <ToDoItems todoList ={todoList} />
   </>
   )
--- react-practise/todo-app/src/components/ToDoForm.jsx
+++ react-practise/todo-app/src/components/ToDoForm.jsx
@@ -1,24 +1,59 @@
-function ToDoForm() {
+import { useState } from "react";
+
+function ToDoForm({ handleAddTodo }) {
+
+    const initialToDo = {
+        name: "",
+        dueDate: new Date().toISOString().substr(0, 10),
+    }
+
+    const [toDoName, setToDoName] = useState(initialToDo);
+
+    const handleToDoName = (e) => {
+        const toDoName = e.target.value;
+
+        const { name, value } = e.target;
+      
+       setToDoName( (prevState) => ({
+        ...prevState,
+        [name] :value 
+       }))
+
+        console.log(toDoName);
+    }
+    const handleAdd = (e) => {
+        e.preventDefault();
+        const newTodo = ({
+            name: toDoName.name,
+            dueDate: toDoName.dueDate
+        });
+        console.log("newTodo After added ", newTodo);
+        handleAddTodo(newTodo) ;
+        setToDoName(initialToDo)
+    }
+
+
+
     return (
         <div>
-          
+
             <div class="container text-center">
-                <div class="row ">
-                    <div class="col-4">
-                       <input type="text" placeholder="Enter ToDo Here"/>
+                <div className="row ">
+                    <div className="col-4">
+                        <input type="text" placeholder="Enter ToDo Here" name="name" value={toDoName.name} onChange={handleToDoName} />
                     </div>
-                    <div class="col-4">
-                        <input type="date"></input>
+                    <div className="col-4">
+                        <input type="date" value={toDoName.dueDate} name="dueDate" onChange={handleToDoName}></input>
                     </div>
-                    <div class="col-4">
-                        <button className=" btn btn-success px-4">Add</button>
+                    <div className="col-4">
+                        <button type="button" className=" btn btn-success px-4 " onClick={handleAdd}>Add</button>
                     </div>
                 </div>
             </div>
 
-          {/* items List  */}
+            {/* items List  */}
 
-        
+
         </div>
     )
 }
